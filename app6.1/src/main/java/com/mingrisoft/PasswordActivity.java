7d66c9761bfcfa04e59cb6f6b17c4d76b0d02ba1
etiger99 <etiger99@163.com>
2020-11-16 02:47:02 +0800

    3rd commit

---
+++ app6.1/src/main/java/com/mingrisoft/PasswordActivity.java
@@ -0,0 +1,26 @@
+package com.mingrisoft;
+
+//import android.app.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password);
+
+        ImageButton close = (ImageButton) findViewById(R.id.close); //获取布局文件中的关闭按钮
+        close.setOnClickListener(new View.OnClickListener() {  //为关闭按钮创建监听事件
+            @Override
+            public void onClick(View v) {
+                finish(); //关闭当前Activity
+            }
+        });
+    }
+}
